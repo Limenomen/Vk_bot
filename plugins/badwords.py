f8a884cc439137c9461d23911d1ecee87cc4dbb5
Your Name <rsagadatov@gmail.com>
2021-08-10 19:33:55 +0500

    10.08.1933

--- plugins/badwords.py
+++ plugins/badwords.py
@@ -1,6 +1,7 @@
 from plugins.base_plugin import Plugin
 from codecs import open
 from random import randint
+from re import match
 
 
 class badwords_plugin(Plugin):
@@ -13,9 +14,10 @@ class badwords_plugin(Plugin):
 
     def start(self, text):
         
-        if text in self.badwords:
-            answer_list = ["Сам такой!!!", "нет, ты!", "а может это ты такой?", "ну ты дурак конечно.", "больше не пиши сюда.", "мда", "по другому не скажешь.", "а кому сейчас легко?", "фу"]
-            random = randint(0, len(answer_list)-1)
-            return (answer_list[random])
-        else:
-            return("Извините, такой команды пока нет(.")
+        for word in self.badwords:
+            if match(rf"{word}", text.lower()):
+                answer_list = ["Сам такой!!!", "нет, ты!", "а может это ты такой?", "ну ты дурак конечно.", "больше не пиши сюда.", "мда", "по другому не скажешь.", "а кому сейчас легко?", "фу"]
+                random = randint(0, len(answer_list)-1)
+                return (answer_list[random])
+            else:
+                return("Извините, такой команды пока нет(.")
